6a4178c85e2964056647c60ea2a29201941fabe7
Abdullah <mdabdullah10301.com>
2026-01-18 01:01:36 +0600

    chore: created eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useffect , useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs"  >
+      <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
