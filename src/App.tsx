9676822831a7500adfaabbe0ff82c9cdd7efdcfc
Abdullah <mdabdullah10301.com>
2026-01-18 00:18:47 +0600

    chore: created eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const unused = 123;
 
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React  </h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
